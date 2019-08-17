f1fbf4f8299c25354a87e07c32ecd56741e210b3
woshiljw <3222413873@qq.com>
2019-08-17 17:32:51 +0800

    The V1.0 ShadowEvaluation code

---
+++ ShadowEvaluation/config.py
@@ -0,0 +1,19 @@
+# Dataset Path
+SBU_test_pth = "/home/wilson/DE/Data/SBU_shadow/SBUTest"
+SBU_train_pth = "/home/wilson/DE/Data/SBU_shadow/SBUTrain4KRecoveredSmall"
+
+ISDT_test_pth = "/home/wilson/DE/Data/ISTD_Dataset/test"
+ISDT_train_pth = "/home/wilson/DE/Data/ISTD_Dataset/train"
+
+UCF_test_pth = ""
+UCF_train_pth = ""
+
+# Restult of detection path
+BDRAR_SBU_Detection = "/home/wilson/DE/Python/BDRAR-master/ckpt/BDRAR/(BDRAR) sbu_prediction_3000"
+BDRAR_UCF_Detection = "/media/wilson/WD_File/wilson/Project/11SoftShadow/Evaluation/18_ECCV_BDRAR_OK/BDRAR_results/UCF"
+
+Direction_SBU_Detection = "/home/wilson/DE/Python/DSC-master/examples/DSC/DSC_detection/result"
+Direction_UCF_Detection = "/media/wilson/WD_File/wilson/Project/11SoftShadow/Evaluation/18_CVPR_Direction_OK/DSC-SBU"
+
+Distraction_SBU_Detection = "/media/wilson/WD_File/wilson/Project/11SoftShadow/Evaluation/19_CVPR_Distraction_OK/code/Output/SBU/models_sbu_prediction_5000_SBU"
+Distraction_UCF_Detection = "/media/wilson/WD_File/wilson/Project/11SoftShadow/Evaluation/19_CVPR_Distraction_OK/results/UCF"
